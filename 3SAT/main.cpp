5a5d612f0fc7a7b08e3a591337871d92ef319848
chris-jantzen <cjantzen22@gmail.com>
2024-03-20 21:27:50 -0400

    3SAT solution passing sample test cases

--- 3SAT/main.cpp
+++ 3SAT/main.cpp
@@ -2,15 +2,14 @@
 #include <utility>
 #include <vector>
 #include <string>
-#include <array>
+#include <map>
 #include <set>
+#include <cmath>
 
-class Literal {
+struct Literal {
 public:
     char variable{};
     bool negated{false};
-
-    Literal(char variable, bool negated) : variable{variable}, negated{negated} {}
 };
 
 class Clause {
@@ -18,6 +17,17 @@ public:
     std::vector<Literal> literals{};
 
     explicit Clause(std::vector<Literal> literals) : literals{std::move(literals)} {}
+
+    [[nodiscard]] bool simulate_clause(const std::map<char, bool> &test_case) const {
+        for (const Literal &l : literals) {
+            bool r = test_case.at(l.variable);
+            if (l.negated)
+                r = !r;
+            if (r)
+                return true;
+        }
+        return false;
+    }
 };
 
 class Formula {
@@ -25,14 +35,75 @@ private:
     std::vector<Clause> clauses{};
     std::set<char> variables{};
 public:
-
     Formula() = default;
-//    explicit Formula(std::vector<Clause> clauses) : clauses{std::move(clauses)} {}
 
+private:
+    std::vector<std::map<char, bool>> populate_permutation(int c) {
+        std::vector<std::map<char, bool>> permutations{};
+        for (int i = 0; i < c; ++i) {
+            std::map<char, bool> chars{};
+            for (auto v : this->variables) {
+                chars[v] = false;
+            }
+            permutations.push_back(chars);
+        }
+        return permutations;
+    }
+
+    static void print_permutations(const std::vector<std::map<char, bool>> &permutations) {
+        for (const std::map<char, bool> &x : permutations) {
+            for (std::pair<char, bool> y : x) {
+                std::cout << y.first << ": " << y.second << " ";
+            }
+            std::cout << '\n';
+        }
+    }
+
+    std::vector<std::map<char, bool>> generateVariablePermutations() {
+        int n = static_cast<int>(this->variables.size());
+        int c = pow(2, n);
+        int x = c/2;
+        int y = 0;
+        bool set{true};
+
+        auto permutations{populate_permutation(c)};
+
+        for (char i : variables) {
+            for(int j = 0; j < c; ++j) {
+                permutations[j].at(i) = set;
+                ++y;
+                if (y == x) {
+                    set = !set;
+                    y = 0;
+                }
+            }
+            x /= 2;
+        }
+
+        return permutations;
+    }
+
+public:
     void solve() {
-        // Create my permutation set which gives values for each variable
-        // Create mechanism to supply the values to each clause in my list of clauses and get the boolean result
-        // Combine all results and accept immediately if there is a permutation that results in all clauses having accepted
+        auto permutations = generateVariablePermutations();
+        for (const auto &p : permutations) {
+            std::vector<bool> result{};
+            for (const Clause &c : this->clauses) {
+                result.push_back(c.simulate_clause(p));
+            }
+            bool accept{true};
+            for (auto r : result) {
+                if (!r) {
+                    accept = false;
+                    break;
+                }
+            }
+            if (accept) {
+                std::cout << "yes" << std::endl;
+                return;
+            }
+        }
+        std::cout << "no" << std::endl;
     }
 
     void read_input() {
@@ -53,7 +124,7 @@ public:
                     negated = true;
                 } else {
                     this->variables.insert(c);
-                    literals.emplace_back(c, negated);
+                    literals.push_back(Literal{c, negated});
                     negated = false;
                 }
             }
@@ -63,37 +134,9 @@ public:
     }
 };
 
-// Read in input
-//Formula read_input() {
-//    std::string n_in;
-//    std::getline(std::cin, n_in);
-//    int n {stoi(n_in)};
-//    std::vector<Clause> clauses{};
-//    for (int i = 0; i < n; ++i) {
-//        std::string clause_line;
-//        std::getline(std::cin, clause_line);
-//
-//        std::vector<Literal> literals{};
-//        bool negated{false};
-//        for (char c : clause_line) {
-//            if (c == ' ') continue;
-//
-//            if (c == '!') {
-//                negated = true;
-//            }
-//            else {
-//                literals.emplace_back(c, negated);
-//                negated = false;
-//            }
-//        }
-//        clauses.emplace_back(literals);
-//    }
-//    return Formula{clauses};
-//}
-
 int main() {
-//    auto formula{read_input()};
     Formula formula{};
     formula.read_input();
+    formula.solve();
     return 0;
 }
